e263a8b153ffa6c85d3fbfd0d262b923475a87ab
Alex Zaharia <alecszaharia@gmail.com>
2017-12-28 15:38:23 +0200

    Small fix

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -6,15 +6,35 @@ class Chessboard extends Component {
         return (
             <div className="chessboard">
                 <div className="boardWrapper">
-                    <div className="row">
-                        <div className="cell">1</div>
-                        <div className="cell">2</div>
-                        <div className="cell">3</div>
-                        <div className="cell">4</div>
-                        <div className="cell">5</div>
-                        <div className="cell">6</div>
-                        <div className="cell">7</div>
-                        <div className="cell">8</div>
+                    <div className="row"> 123
+                        {/*<div className="cell">1</div>*/}
+                        {/*<div className="cell">2</div>*/}
+                        {/*<div className="cell">3</div>*/}
+                        {/*<div className="cell">4</div>*/}
+                        {/*<div className="cell">5</div>*/}
+                        {/*<div className="cell">6</div>*/}
+                        {/*<div className="cell">7</div>*/}
+                        {/*<div className="cell">8</div>*/}
+                    </div>
+                    <div className="row">123
+                        {/*<div className="cell">1</div>*/}
+                        {/*<div className="cell">2</div>*/}
+                        {/*<div className="cell">3</div>*/}
+                        {/*<div className="cell">4</div>*/}
+                        {/*<div className="cell">5</div>*/}
+                        {/*<div className="cell">6</div>*/}
+                        {/*<div className="cell">7</div>*/}
+                        {/*<div className="cell">8</div>*/}
+                    </div>
+                    <div className="row">123
+                        {/*<div className="cell">1</div>*/}
+                        {/*<div className="cell">2</div>*/}
+                        {/*<div className="cell">3</div>*/}
+                        {/*<div className="cell">4</div>*/}
+                        {/*<div className="cell">5</div>*/}
+                        {/*<div className="cell">6</div>*/}
+                        {/*<div className="cell">7</div>*/}
+                        {/*<div className="cell">8</div>*/}
                     </div>
 
                 </div>
